8c7dc9f783aadf7b3c5c3839eb90f96b676e2721
GerardoCalistenia <318245630@ciencias.unam.mx>
2023-10-09 00:41:19 +0000

    conecto bd

--- src/app/logicApp/settings.py
+++ src/app/logicApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'logicApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'usuarios',
+        'USER': 'postgres',
+        'PASSWORD': 'boguetos',
+        'HOST': '10.0.0.4',
+        'PORT': '5432',
     }
 }
 
