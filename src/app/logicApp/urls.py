9a5463a0551d0b0bfec571ee2954dfe153e85b3c
GerardoCalistenia <318245630@ciencias.unam.mx>
2023-10-09 00:32:50 +0000

    trabajo en logica de login

--- src/app/logicApp/urls.py
+++ src/app/logicApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from redes_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('submit-login/', views.submit_login, name='submit_login')
 ]
